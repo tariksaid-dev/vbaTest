890f85691de495b776e5dc14a4c06308ffdaad44
miriam.romero <miriam.romero@civica-soft.com>
2024-03-15 12:07:02 +0100

    Add header and id, level, points and killer columns in Q&A sheet

--- macro-excel-sql test.vb
+++ macro-excel-sql test.vb
@@ -1,11 +1,19 @@
 Sub GenerarInformeConID()
     Dim wbOriginalData As Workbook, wbNuevo As Workbook
-    Dim wsOriginalData As Worksheet, wsNuevo As Worksheet
+    Dim wsOriginalData As Worksheet, wsTemplate As Worksheet, wsPreguntasRespuestas As Worksheet, wsResumen As Worksheet
     Dim rutaOriginalData As String
     Dim i As Long, destinoFila As Long, filaID As Long
     Dim idUsuario As Variant
     Dim encontrado As Range
 
+    ' Propiedades para la hoja de Preguntas y Respuestas
+    Dim id As String
+    Dim level As String
+    Dim points As Integer
+    Dim killer As Integer
+    
+    ' Propiedades para la hoja de Resumen
+    
     ' Solicitar al usuario que introduzca el ID
     idUsuario = Application.InputBox("Introduce el ID para buscar:", "Buscar por ID", Type:=1)
 
@@ -16,7 +24,8 @@ Sub GenerarInformeConID()
     End If
 
     ' Ruta completa del archivo de producción
-    rutaOriginalData = "C:\Users\tarik.said\Desktop\vbaTest\Cuestionario SQL (Producción).xlsx"
+    ' rutaOriginalData = "C:\Users\tarik.said\Desktop\vbaTest\Cuestionario SQL (Producción).xlsx"
+    rutaOriginalData = "C:\Users\miriam.romero\Documents\macro-excel\vbaTest\Cuestionario SQL (Producción).xlsx"
 
     ' Intentar abrir el libro de producción
     Set wbOriginalData = Workbooks.Open(rutaOriginalData)
@@ -28,17 +37,37 @@ Sub GenerarInformeConID()
     ' Verificar si se encontró el ID
     If Not encontrado Is Nothing Then
         filaID = encontrado.Row
+        Debug.Print "El valor de filaID es: " & filaID
     Else
         MsgBox "El ID que has introducido no existe.", vbExclamation
         wbOriginalData.Close SaveChanges:=False
         Exit Sub
     End If
 
+    Set wsTemplate = ThisWorkbook.Sheets("answer template")
+
     ' Crear un nuevo libro para los resultados
     Set wbNuevo = Workbooks.Add
-    Set wsNuevo = wbNuevo.Sheets(1)
+    
+    Set wsPreguntasRespuestas = wbNuevo.Sheets(1)
+    wbNuevo.Sheets(1).Name = "Preguntas y respuestas"
+    
+    Set wsResumen = wbNuevo.Worksheets.Add(After:=wbNuevo.Sheets(1))
+    wsResumen.Name = "Resumen"
 
-    destinoFila = 1 ' Iniciar en la primera fila para los datos copiados
+    ' ##### Acciones en la hoja de Preguntas y respuestas #####
+
+    ' Cabeceras
+    wsPreguntasRespuestas.Cells(1, 1).Value = "question"
+    wsPreguntasRespuestas.Cells(1, 2).Value = "user answer"
+    wsPreguntasRespuestas.Cells(1, 3).Value = "id"
+    wsPreguntasRespuestas.Cells(1, 4).Value = "Level"
+    wsPreguntasRespuestas.Cells(1, 5).Value = "Points"
+    wsPreguntasRespuestas.Cells(1, 6).Value = "Killer answer"
+    
+    wsPreguntasRespuestas.Columns("C").NumberFormat = "@"
+
+    destinoFila = 2 ' Iniciar en la segunda fila para los datos copiados tras las cabeceras
 
     ' Determinar la última columna con datos en la fila 1
     Dim ultimaColumna As Long
@@ -49,17 +78,46 @@ Sub GenerarInformeConID()
         ' Verificar si el valor de la celda comienza con "["
         If Left(wsOriginalData.Cells(1, i).Value, 1) = "[" Then
             ' Transponer los valores de la fila 1 y la fila encontrada a las columnas A y B en el nuevo libro
-            wsNuevo.Cells(destinoFila, 1).Value = Trim(wsOriginalData.Cells(1, i).Value)
-            wsNuevo.Cells(destinoFila, 2).Value = Trim(wsOriginalData.Cells(filaID, i).Value)
+            wsPreguntasRespuestas.Cells(destinoFila, 1).Value = Trim(wsOriginalData.Cells(1, i).Value)
+            wsPreguntasRespuestas.Cells(destinoFila, 2).Value = Trim(wsOriginalData.Cells(filaID, i).Value)
+
+            ' Obtener ID
+            id = Mid(wsOriginalData.Cells(1, i).Value, 2, 4) & "." & Mid(wsOriginalData.Cells(filaID, i).Value, 2, 2)
+            wsPreguntasRespuestas.Cells(destinoFila, 3).Value = id
+            
+            ' Obtener Level
+            level = WorksheetFunction.VLookup(id, wsTemplate.Range("C:F"), 2, False)
+            wsPreguntasRespuestas.Cells(destinoFila, 4).Value = level
+            
+            ' Obtener Points
+            points = WorksheetFunction.VLookup(id, wsTemplate.Range("C:F"), 3, False)
+            wsPreguntasRespuestas.Cells(destinoFila, 5).Value = points
+            
+            ' Obtener Killer answer
+            killer = WorksheetFunction.VLookup(id, wsTemplate.Range("C:F"), 4, False)
+            wsPreguntasRespuestas.Cells(destinoFila, 6).Value = killer
+
             destinoFila = destinoFila + 1
         End If
     Next i
+    
+    ' ##### Acciones en la hoja de Resumen #####
+    
+    ' Dibujar gráfico
+    
+    ' Pintar pequeño cuadro resumen
+    
+    ' Rellenar tabla
 
-    ' Guardar el nuevo libro con los resultados
-    wbNuevo.SaveAs "C:\Users\tarik.said\Desktop\vbaTest\Resultados.xlsx"
+    ' ##### Guardar archivo resultante #####
+    
+    ' wbNuevo.SaveAs "C:\Users\tarik.said\Desktop\vbaTest\Resultados.xlsx"
+    wbNuevo.SaveAs "C:\Users\miriam.romero\Documents\macro-excel\vbaTest\Resultados SQL - Usuario Prueba.xlsx"
 
     ' Cerrar el libro de producción sin guardar cambios
     wbOriginalData.Close SaveChanges:=False
 
-    MsgBox "Informe generado con éxito en: C:\Users\tarik.said\Desktop\vbaTest\Resultados.xlsx", vbInformation
+    MsgBox "Informe generado con éxito en: C:\Users\miriam.romero\Documents\macro-excel\vbaTest\Resultados.xlsx", vbInformation
 End Sub
+
+
